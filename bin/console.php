797f87bcf911594843c5d94df13901db9affa5f6
Andi Rückauer <arueckauer@gmail.com>
2022-10-19 10:52:46 +0200

    Adds service retrieval via auto-wiring

--- bin/console.php
+++ bin/console.php
@@ -2,9 +2,15 @@
 
 declare(strict_types=1);
 
+use App\ServiceA;
 use Psr\Container\ContainerInterface;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $container = require dirname(__DIR__) . '/config/container.php';
 assert($container instanceof ContainerInterface);
+
+$serviceA = $container->get(ServiceA::class);
+assert($serviceA instanceof ServiceA);
+
+echo $serviceA->invoke() . PHP_EOL;
