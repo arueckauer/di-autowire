f91848f5b97bdaa64db49f1080972e900916de72
Andi Rückauer <arueckauer@gmail.com>
2022-10-19 10:51:42 +0200

    Adds dependencies and config provided by AutoWireConfigBuilder to application configuration

--- config/config.php
+++ config/config.php
@@ -2,12 +2,29 @@
 
 declare(strict_types=1);
 
+use Laminas\ConfigAggregator\ArrayProvider;
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
+use OpsWay\Laminas\ServiceManager\Attributes\AutoWireConfigBuilder;
+use OpsWay\Laminas\ServiceManager\Attributes\DI\AttributeScanner;
+
+$cacheConfig = [
+    'config_cache_path' => 'data/cache/config-cache.php',
+];
+
+$builder = new AutoWireConfigBuilder(
+    new AttributeScanner('App')
+);
 
 $aggregator = new ConfigAggregator([
     // App
     App\ConfigProvider::class,
+
+    // AutoWire
+    new ArrayProvider([
+        'dependencies' => $builder->getDependencies(),
+    ]),
+    new ArrayProvider($builder->getConfig()),
 
     // Configuration
     new PhpFileProvider(realpath(__DIR__) . '/autoload/{{,*.}global,{,*.}local}.php'),
